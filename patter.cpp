2813def49645436b67e2c86197e5bbd210273598
Khushi98 <khushi.rana7689@gmail.com>
2026-01-22 05:16:53 +0000

    Pattern priniting

--- patter.cpp
+++ patter.cpp
@@ -28,11 +28,41 @@ cout<<endl;}
 
 // Pattern 4:
 cout<<"Pattern 4: "<<endl;
+for(char row='a';row<='e';row++){
+    
+    for(int col=1;col<=5;col++){
+        cout<<row<<" ";
+    }
+    cout<<endl;
+}
+
+// Mthod2:
 for(int row=1;row<=5;row++){
-    char name='a'+(row-1);
+    char name ='a'+(row-1);
     for(int col=1;col<=5;col++){
         cout<<name<<" ";
     }
     cout<<endl;
 }
+// Pattern 5: a b c d e
+            //a b c d e
+cout<<endl<<"Pattern 5: ";
+for(int row=1;row<=5;row++){
+for(char c='a';c<='e';c++){
+    
+    cout<<c<<" ";
+}
+cout<<endl;
+}
+
+// Pattern 6:
+// 1 2 3 4 5
+// 6 7 8 9 10
+for(int row=1;row<=5;row++){
+    for(int col=1;col<=5;col++){
+        cout<<5*(row-1)+col<<" ";
+    }
+    cout<<endl;
+}
+
 }
