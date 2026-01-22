7b2b381541681b4e7f09e522cfa85b6de33f1b1b
Khushi98 <khushi.rana7689@gmail.com>
2026-01-22 06:56:37 +0000

    star printing pattern

--- Star.cpp
+++ Star.cpp
@@ -84,13 +84,11 @@ int main(){
     }
 
     cout<<endl<<"Right angled triangle variation"<<endl;
-    // for(int row=1;row<=5;row++){
-    //     int num=5;
-    //     for(int col=1;col<=row;col--){
-            
-    //         cout<< num--<<" ";
-    //     }
-    //     cout<<endl;
-    // }
+    for(int row=1;row<=5;row++){
+        for(int col=5;col>=5-(row-1);col--){
+            cout<<col<<" ";
+        }
+        cout<<endl;
+    }
     return 0;
 }
