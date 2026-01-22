1917ced97dfacb95fe6d79d7125b47359d0a2668
Khushi98 <khushi.rana7689@gmail.com>
2026-01-22 04:31:57 +0000

    pattern

---
+++ patter.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+int main(){
+//    Pattern1:
+  for(int i=0;i<=5;i++){
+      for(int j=0;j<=4;j++){
+          cout<<"*"<<" ";
+      }
+      cout<<endl;
+  }
+
+//   Pattern2:
+cout<<endl;
+cout<<"Pattern 2: "<<endl;
+for(int j=1;j<=5;j++){
+for(int i=1;i<=5;i++){
+    cout<<j<<" ";
+}
+cout<<endl;}
+
+// Pattern 3:
+cout<<"Pattern 3"<<endl;
+for(int r=1;r<=5;r++){
+for(int c=5;c>=1;c--){
+    cout<<c<<" ";
+}
+cout<<endl;}
+}
